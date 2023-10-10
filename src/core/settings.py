c10674ba162e87e1e5b923d87041639a35932ffe
Ilkhan <ilhanchanel20002@gmail.com>
2023-10-10 16:54:23 +0300

    .

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
-    'https://admin.savewithsports.xyz',
+    'https://admin.savewithsports.com',
     'http://localhost:8000'
 ]
 
